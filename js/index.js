c4c516feff08be6c92ca81b66c011642dffb9540
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-03-06 03:43:44 -0300

    removendo aba de about adicionando texto no index e icones

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
 $(function() {
     $('#header').load('./templates/header.html');
-    // $('#footer').load('footer.html')
-
-    // <span class="sr-only">(current)</span>
+    $('[data-toggle="popover"]').popover();
 });
